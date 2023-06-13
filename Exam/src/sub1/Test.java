9867913c5c5e4ed061397c6fc220a75920b13e02
kim2354 <136422023+kim2354@users.noreply.github.com>
2023-06-13 17:47:55 +0900

    Add files via upload

---
+++ Exam/src/sub1/Test.java
@@ -0,0 +1,17 @@
+package test1;
+/* 날짜 : 2023/06/13
+ * 이름 : 김건우	
+ * 내용 : 자바 자료형 연습문제
+ */
+public class Test01{
+	public static void main(String[] args) {
+		
+	
+	int vara= 'a';
+	byte var100 = 100;
+	long var3 = 2147483648L;
+	float var4 = 3.14f;
+	double var5 = 3.14159;
+	boolean var6 = true;
+	boolean var7 = false;
+	
