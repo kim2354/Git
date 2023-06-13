517547aec060eac9496b34f379f1ea7878199b3c
kim2354 <136422023+kim2354@users.noreply.github.com>
2023-06-13 16:50:23 +0900

    Add files via upload

---
+++ Java/src/sub2/calculator.java
@@ -0,0 +1,15 @@
+package sub2;
+
+public class calculator {
+	public static void main(String args) {
+
+		int x = 1; // 변수 x 를 선언하고 1저장
+
+		int y = 2; // 변수 y 를 선언하고 2저장
+
+		int result = x + y; // 변수 result를 선언하고 x 와 y 더한값을 저장
+
+		System.out.println(result); //콘솔에 출력하는 메소드 호출
+
+		}
+}
